3f217c7451759d76c83a41aaca9fa65edd747447
rexazier <65490297+rexazier@users.noreply.github.com>
2022-05-09 10:34:38 +0700

    Update and rename unix2base36.py to unix2base62.py

--- unix2base62.py
+++ unix2base62.py
@@ -1,31 +1,35 @@
-import sys
-import datetime
 import time
 
 def get_digit(d):
-    ''' Convert a base 36 digit to the desired character '''
+    ''' Convert a base 62 digit to the desired character '''
     if 0 <= d <= 9:
         # 0 - 9
         c = 48 + d
     elif 10 <= d <= 35:
         # A - Z
         c = (65-10) + d
+    elif 36 <= d <= 61:
+        # a - z
+        c = (97-36) + d
     else:
         # We should never get here
-        raise ValueError('Invalid digit for base 36: ' + str(d)) 
+        raise ValueError('Invalid digit for base 62: ' + str(d)) 
     return chr(c)
 
+# Test `digit`
+#print(''.join([get_digit(d) for d in range(62)]))
+
 def encode(n):
-    ''' Convert integer n to base 36 '''
+    ''' Convert integer n to base 62 '''
     out = []
     while n:
-        n, r = n // 36, n % 36
+        n, r = n // 62, n % 62
         out.append(get_digit(r))
     while len(out) < 6:
         out.append('0')
     return ''.join(out)[::-1]
 
-def unix2base36():
+def unix2base62():
     unix_now = time.time()
     #date_time = datetime.datetime(2000, 1, 1, 0, 0)
     #unix_start = time.mktime(date_time.timetuple())
@@ -34,4 +38,4 @@ def unix2base36():
     return encode(ms)
 
 def timename():
-    return unix2base36()
+    return unix2base62()
